0a522caac6dd89bda8c459e731cc8932e3ec4bda
Markus N <markus.niederbrucker@spinnwerk.at>
2023-12-01 08:59:09 +0100

    ➕ adding vendor directory to version control

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'spinnwerk/finance-a-bike',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'c4d0c5859ea91f0699b947ecd845ca0e320a77d1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'spinnwerk/finance-a-bike' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'c4d0c5859ea91f0699b947ecd845ca0e320a77d1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'yahnis-elsts/plugin-update-checker' => array(
+            'pretty_version' => 'v5.0',
+            'version' => '5.0.0.0',
+            'reference' => '81be284da76f12c8751b477b2a0fa44364d26f84',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../yahnis-elsts/plugin-update-checker',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
